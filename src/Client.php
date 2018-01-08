22f670f3873005052d0464a82727851d2bf4057a
Matthew Erskine <matthew@erskine.com>
2018-01-08 10:44:45 +0000

    Use OAuth style grant in helper client

--- src/Client.php
+++ src/Client.php
@@ -1,10 +1,15 @@
 <?php
 namespace BingDeCrapperWrapper;
 
+use Microsoft\BingAds\Auth\ServiceClient;
 use Microsoft\BingAds\Auth\ApiEnvironment;
 use Microsoft\BingAds\Auth\AuthorizationData;
+use Microsoft\BingAds\Auth\ServiceClientType;
+use Microsoft\BingAds\Auth\OAuthAuthorization;
+use Microsoft\BingAds\Auth\OAuthWebAuthCodeGrant;
 use Microsoft\BingAds\Auth\PasswordAuthentication;
-use Microsoft\BingAds\Auth\ServiceClient;
+use Microsoft\BingAds\Auth\OAuthDesktopMobileAuthCodeGrant;
+use Microsoft\BingAds\Auth\OAuthDesktopMobileImplicitGrant;
 
 class Client
 {
@@ -20,18 +25,23 @@ class Client
      * @param string $service
      */
     public function __construct(
-        string $userName,
-        string $password,
+        string $clientId,
+        string $clientSecret,
+        string $refreshToken,
         string $developerToken,
-        string $service
+        string $service = null
     ) {
-        $passwordAuthentication = new PasswordAuthentication();
-        $passwordAuthentication->withUserName($userName);
-        $passwordAuthentication->withPassword($password);
+        $service = $service ?? ServiceClientType::ReportingVersion11;
 
-        $authentication = new AuthorizationData();
-        $authentication->withAuthentication($passwordAuthentication);
-        $authentication->withDeveloperToken($developerToken);
+        $authentication = (new OAuthDesktopMobileAuthCodeGrant())
+            ->withClientId($clientId)
+            ->withClientSecret($clientSecret);
+
+        $authentication = (new AuthorizationData())
+            ->withAuthentication($authentication)
+            ->withDeveloperToken($developerToken);
+
+        $authentication->Authentication->RequestOAuthTokensByRefreshToken($refreshToken);
 
         $this->client = new ServiceClient($service, $authentication, ApiEnvironment::Production);
     }
